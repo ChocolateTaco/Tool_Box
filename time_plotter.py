1a36b5ba6227e3cb50ed63e9b1a2d2a4fe192d13
Steven Tran <trans4@oregonstate.edu>
2022-06-04 09:50:31 -0700

    sort plotter updated with shell, merge, insertion

---
+++ time_plotter.py
@@ -0,0 +1,140 @@
+# Author:           Steven Tran
+# Date:             2021-11-17
+# Description:      This program plots the time it takes for bubble sort and
+# insertion sort to finish sorting the same set of data (a list in this
+# case). It uses the matplotlib module to graph the results of 10 different
+# sized randomly generated data sets.
+
+# Modules Imports
+import time
+import random
+import functools
+from matplotlib import pyplot
+from shell_sort import shellSort
+from selection_sort import selectionSort
+from merge_sort import mergeSort
+
+def sort_timer(func):
+	"""Wrapper for timing the duration a function takes to run."""
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		"""Prints out the total elapsed time."""
+		start = time.perf_counter()
+		# print(f"Start time: {start}")
+		func(*args, **kwargs)
+		end = time.perf_counter()
+		# print(f"End time: {end}")
+		total_elapsed_time = (end - start)
+		print(f"Total elapsed time: {total_elapsed_time:0.6f} seconds")
+		return total_elapsed_time
+	return wrapper
+
+@sort_timer
+def bubble_sort(a_list):
+	"""Sorts a_list in ascending order.
+	Note: From Module 4"""
+	for pass_num in range(len(a_list) - 1):
+		for index in range(len(a_list) - 1 - pass_num):
+			if a_list[index] > a_list[index + 1]:
+				temp = a_list[index]
+				a_list[index] = a_list[index + 1]
+				a_list[index + 1] = temp
+
+@sort_timer
+def insertion_sort(a_list):
+	"""Sorts a_list in ascending order.
+	Note: From Module 4"""
+	for index in range(1, len(a_list)):
+		value = a_list[index]
+		pos = index - 1
+		while pos >= 0 and a_list[pos] > value:
+			a_list[pos + 1] = a_list[pos]
+			pos -= 1
+		a_list[pos + 1] = value
+
+@sort_timer
+def mSortHere(a_list):
+	mergeSort(a_list)
+
+@sort_timer
+def selSort(a_list):
+	selectionSort(a_list)
+
+@sort_timer
+def shellS(a_list):
+	shellSort(a_list)
+
+@sort_timer
+def compare_sorts(sorting_alg1=bubble_sort, sorting_alg2=insertion_sort,
+sorting_alg3=mSortHere, sorting_alg4=selSort, sorting_alg5=shellS):
+	"""Will compare the two sorting functions: bubble sort and insertion
+	sort."""
+	# Initializing the sorting algorithm's x and y values
+	x_axis = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+	alg1_ytime = []
+	alg2_ytime = []
+	alg3_ytime = []
+	alg4_ytime = []
+	alg5_ytime = []
+	# Initializing the number of points for the list of random #s.
+	point_count = 1000
+	point_count_max = 10000
+	point_increment = 1000
+
+	while point_count <= point_count_max:
+		list_1 = []         # first list to be randomized by the plot limit
+		for i in range(point_count):
+			list_1.append(random.randint(1, 10000)) # randomizes value
+		# between 1 and 10000
+		list_2 = list_1.copy() # copy of the first list
+		list_3 = list_1.copy()
+		list_4 = list_1.copy()
+		list_5 = list_1.copy()
+
+		# Sorting begins + info for human to know status.
+		sort1 = sorting_alg1(list_1)
+		alg1_ytime.append(sort1)
+		print(f"* Completed {sorting_alg1.__name__} with {point_count} "
+		      f"points")
+		sort2 = sorting_alg2(list_2)
+		alg2_ytime.append(sort2)
+		print(f"* Completed {sorting_alg2.__name__} with {point_count} "
+		      f"points\n")
+		sort3 = sorting_alg3(list_3)
+		alg3_ytime.append(sort3)
+		print(f"* Completed {sorting_alg3.__name__} with {point_count} "
+		      f"points\n")
+		sort4 = sorting_alg4(list_4)
+		alg4_ytime.append(sort4)
+		print(f"* Completed {sorting_alg4.__name__} with {point_count} "
+		      f"points\n")
+		sort5 = sorting_alg5(list_5)
+		alg5_ytime.append(sort5)
+		print(f"* Completed {sorting_alg5.__name__} with {point_count} "
+		      f"points\n")
+		point_count += point_increment
+
+	# Plot Section that plots Number of Elements Sorted versus the Time
+	pyplot.plot(x_axis, alg1_ytime, 'co--', linewidth=2,
+	            label=f"{sorting_alg1.__name__}")
+	pyplot.plot(x_axis, alg2_ytime, 'go--', linewidth=2,
+	            label=f"{sorting_alg2.__name__}")
+	pyplot.plot(x_axis, alg3_ytime, 'ro--', linewidth=2,
+	            label=f"{sorting_alg3.__name__}")
+	pyplot.plot(x_axis, alg4_ytime, 'yo--', linewidth=2, 
+	            label=f"{sorting_alg4.__name__}")
+	pyplot.plot(x_axis, alg5_ytime, 'bo--', linewidth=2,
+	            label=f"{sorting_alg5.__name__}")
+	pyplot.xlabel("Number of Elements Sorted")
+	pyplot.ylabel("Time to Sort (seconds)")
+	pyplot.title("Sorting Algorithm Comparison")
+	pyplot.legend()
+	pyplot.grid()
+	pyplot.show()
+
+def main():
+	"""Per the read me, to make things a little more efficient for the TAs :)"""
+	compare_sorts()
+
+if __name__ == "__main__":
+	main()
